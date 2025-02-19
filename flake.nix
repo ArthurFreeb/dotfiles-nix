b7a1061e6c26ad9e88dffbe29e074b798bbf412c
ArthurFreeb <32460970+ArthurFreeb@users.noreply.github.com>
2025-02-19 09:36:08 +0000

    wip

--- flake.nix
+++ flake.nix
@@ -43,6 +43,12 @@
           modules = [ ./home-manager/james/mac.nix ];
           extraSpecialArgs = { inherit self; };
         };
+
+        "arthur@wsl" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [ ./home-manager/arthur/wsl.nix ];
+          extraSpecialArgs = { inherit self; };
+        };
       };
     };
 }
